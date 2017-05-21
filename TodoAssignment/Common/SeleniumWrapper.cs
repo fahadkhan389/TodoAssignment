0d75e94792a4d663b8e75c0130c0de6d1a519502
fukhan <fukhan389@hotmail.com>
2017-05-21 12:06:37 +1000

    commit

--- TodoAssignment/Common/SeleniumWrapper.cs
+++ TodoAssignment/Common/SeleniumWrapper.cs
@@ -19,128 +19,198 @@ namespace TodoAssignment
     {
         
 
-        public static void FilltextBox(string element, string value, IdentityType Elementtype)
+        public static void FilltextBox(IWebElement element, string value)
         {
-            switch (Elementtype)
-            {
-                case IdentityType.Id:
-                    PropertiesCollection.driver.FindElement(By.Id(element)).SendKeys(value);
-                    break;
-                case IdentityType.Name:
-                    PropertiesCollection.driver.FindElement(By.Name(element)).SendKeys(value);
-                    break;
-                case IdentityType.Xpath:
-                    PropertiesCollection.driver.FindElement(By.XPath(element)).SendKeys(value);
-                    break;
+                 element.SendKeys(value);
+           
+        }
+
+        //public static void FilltextBox(string element, string value, IdentityType Elementtype)
+        //{
+        //    switch (Elementtype)
+        //    {
+        //        case IdentityType.Id:
+        //            PropertiesCollection.driver.FindElement(By.Id(element)).SendKeys(value);
+        //            break;
+        //        case IdentityType.Name:
+        //            PropertiesCollection.driver.FindElement(By.Name(element)).SendKeys(value);
+        //            break;
+        //        case IdentityType.Xpath:
+        //            PropertiesCollection.driver.FindElement(By.XPath(element)).SendKeys(value);
+        //            break;
+
+        //    }
+
+
+        //}
+
+        //public static IWebElement FindElement(string element, IdentityType Elementtype)
+        //{
+        //    IWebElement Webelement;
+        //    switch (Elementtype)
+        //    {
+        //        case IdentityType.Id:
+        //            Webelement = PropertiesCollection.driver.FindElement(By.Id(element));
+        //            break;
+        //        case IdentityType.Name:
+        //            Webelement = PropertiesCollection.driver.FindElement(By.Name(element));
+        //            break;
+        //        case IdentityType.Xpath:
+        //            Webelement = PropertiesCollection.driver.FindElement(By.XPath(element));
+        //            break;
+        //        default:
+        //            throw new Exception();
+        //    }
+        //    return Webelement;
+        //}
+        public static Boolean IsElementDisplayed(IWebElement element)
+
+        {
+            return (element.Displayed && element.Enabled);
+            
+        }
+        //public static Boolean IsElementDisplayed(string element, IdentityType Elementtype)
+
+        //{
+        //    IWebElement webelement = FindElement(element, Elementtype);
+        //    return (webelement.Displayed && webelement.Enabled);
+
+        //}
+        //public static void EnterFillTextBox(string element, string value, IdentityType Elementtype)
+        //{
+        //    switch (Elementtype)
+        //    {
+        //        case IdentityType.Id:
+        //            PropertiesCollection.driver.FindElement(By.Id(element)).SendKeys(value);
+        //            PropertiesCollection.driver.FindElement(By.Id(element)).SendKeys(Keys.Enter);
+
+        //            break;
+        //        case IdentityType.Name:
+        //            PropertiesCollection.driver.FindElement(By.Name(element)).SendKeys(value);
+        //            PropertiesCollection.driver.FindElement(By.Name(element)).SendKeys(Keys.Enter);
+
+        //            break;
+        //        case IdentityType.Xpath:
+        //            PropertiesCollection.driver.FindElement(By.XPath(element)).SendKeys(value);
+        //            PropertiesCollection.driver.FindElement(By.XPath(element)).SendKeys(Keys.Enter);
+
+        //            break;
+        //    }
+        //}
+
+        public static void EnterFillTextBox(IWebElement element, string value)
+        {
+                    element.SendKeys(value);
+                    element.SendKeys(Keys.Enter);
                     
-            }
         }
-        public static void EnterFillTextBox(string element, string value, IdentityType Elementtype)
-        {
-            switch (Elementtype)
-            {
-                case IdentityType.Id:
-                    PropertiesCollection.driver.FindElement(By.Id(element)).SendKeys(value);
-                    PropertiesCollection.driver.FindElement(By.Id(element)).SendKeys(Keys.Enter);
 
-                    break;
-                case IdentityType.Name:
-                    PropertiesCollection.driver.FindElement(By.Name(element)).SendKeys(value);
-                    PropertiesCollection.driver.FindElement(By.Name(element)).SendKeys(Keys.Enter);
-
-                    break;
-                case IdentityType.Xpath:
-                    PropertiesCollection.driver.FindElement(By.XPath(element)).SendKeys(value);
-                    PropertiesCollection.driver.FindElement(By.XPath(element)).SendKeys(Keys.Enter);
-
-                    break;
-            }
-        }
-        public static void DoubleClickElement(string element, IdentityType Elementtype)
+        public static void DoubleClickElement(IWebElement element)
         {
 
-            switch (Elementtype)
-            {
-
-                case IdentityType.Xpath:
-                    Actions builder1 = new Actions(PropertiesCollection.driver);
-                    IWebElement webelement1 = PropertiesCollection.driver.FindElement(By.XPath(element));
-
-                    builder1.DoubleClick(webelement1).Build().Perform();
-                    break;
-                case IdentityType.Name:
-                    Actions builder2 = new Actions(PropertiesCollection.driver);
-                    IWebElement webelement2 = PropertiesCollection.driver.FindElement(By.Name(element));
-
-                    builder2.DoubleClick(webelement2).Build().Perform();
-                    break;
-                case IdentityType.Id:
-                    Actions builder3 = new Actions(PropertiesCollection.driver);
-                    IWebElement webelement3 = PropertiesCollection.driver.FindElement(By.Id(element));
-
-                    builder3.DoubleClick(webelement3).Build().Perform();
-                    break;
-            }
-        }
-        public static void ClickElement(string element, IdentityType Elementtype)
-        {
-
-            switch (Elementtype)
-            {
-
-                case IdentityType.Id:
-                    PropertiesCollection.driver.FindElement(By.Id(element)).Click();
-
-                    break;
-                case IdentityType.Name:
-                    PropertiesCollection.driver.FindElement(By.Name(element)).Click();
-
-                    break;
-                case IdentityType.Xpath:
-                    PropertiesCollection.driver.FindElement(By.XPath(element)).Click();
-
-                    break;
-
-            }
-        }
-        public static string GetText(string element, IdentityType Elementtype)
-        {
-
-            switch (Elementtype)
-            {
-
-                case IdentityType.Id:
-                    return PropertiesCollection.driver.FindElement(By.Id(element)).GetAttribute("value");
-
-                case IdentityType.Name:
-                    return PropertiesCollection.driver.FindElement(By.Name(element)).GetAttribute("value");
-
-                case IdentityType.Xpath:
-                    return PropertiesCollection.driver.FindElement(By.XPath(element)).GetAttribute("value");
-
+                   Actions builder1 = new Actions(PropertiesCollection.driver);
                     
-                default: return "default";
-
+                    builder1.DoubleClick(element).Build().Perform();
+                   
             }
+        //switch (Elementtype)
+        //{
+
+        //    case IdentityType.Xpath:
+        //        Actions builder1 = new Actions(PropertiesCollection.driver);
+        //        IWebElement webelement1 = PropertiesCollection.driver.FindElement(By.XPath(element));
+
+        //        builder1.DoubleClick(webelement1).Build().Perform();
+        //        break;
+        //    case IdentityType.Name:
+        //        Actions builder2 = new Actions(PropertiesCollection.driver);
+        //        IWebElement webelement2 = PropertiesCollection.driver.FindElement(By.Name(element));
+
+        //        builder2.DoubleClick(webelement2).Build().Perform();
+        //        break;
+        //    case IdentityType.Id:
+        //        Actions builder3 = new Actions(PropertiesCollection.driver);
+        //        IWebElement webelement3 = PropertiesCollection.driver.FindElement(By.Id(element));
+
+        //        builder3.DoubleClick(webelement3).Build().Perform();
+        //        break;
+        //}
+
+
+        //public static void ClickElement(string element, IdentityType Elementtype)
+        //{
+
+        //    switch (Elementtype)
+        //    {
+
+        //        case IdentityType.Id:
+        //            PropertiesCollection.driver.FindElement(By.Id(element)).Click();
+
+        //            break;
+        //        case IdentityType.Name:
+        //            PropertiesCollection.driver.FindElement(By.Name(element)).Click();
+
+        //            break;
+        //        case IdentityType.Xpath:
+        //            PropertiesCollection.driver.FindElement(By.XPath(element)).Click();
+
+        //            break;
+
+        //    }
+        //}
+        public static void ClickElement(IWebElement element)
+        {
+            element.Click();
+            
         }
+        public static string GetText(IWebElement element)
+        {
+            return element.GetAttribute("value");
 
-            public static void ClearTextBox(string element, IdentityType Elementtype)
+               }
+        //public static string GetText(string element, IdentityType Elementtype)
+        //{
+
+        //    switch (Elementtype)
+        //    {
+
+        //        case IdentityType.Id:
+        //            return PropertiesCollection.driver.FindElement(By.Id(element)).GetAttribute("value");
+
+        //        case IdentityType.Name:
+        //            return PropertiesCollection.driver.FindElement(By.Name(element)).GetAttribute("value");
+
+        //        case IdentityType.Xpath:
+        //            return PropertiesCollection.driver.FindElement(By.XPath(element)).GetAttribute("value");
+
+
+        //        default: return "default";
+
+        //    }
+        //}
+        public static void ClearTextBox(IWebElement element)
             {
-
-                switch (Elementtype)
-                {
-
-                    case IdentityType.Id:
-                    PropertiesCollection.driver.FindElement(By.Id(element)).Clear();
-                    break;
-                    case IdentityType.Name:
-                    PropertiesCollection.driver.FindElement(By.Name(element)).Clear();
-                    break;
-                    case IdentityType.Xpath:
-                    PropertiesCollection.driver.FindElement(By.XPath(element)).Clear();
-                    break;
+              element.Clear();
+                    
                 }
 
+
+            //public static void ClearTextBox(string element, IdentityType Elementtype)
+            //{
+
+            //    switch (Elementtype)
+            //    {
+
+            //        case IdentityType.Id:
+            //            PropertiesCollection.driver.FindElement(By.Id(element)).Clear();
+            //            break;
+            //        case IdentityType.Name:
+            //            PropertiesCollection.driver.FindElement(By.Name(element)).Clear();
+            //            break;
+            //        case IdentityType.Xpath:
+            //            PropertiesCollection.driver.FindElement(By.XPath(element)).Clear();
+            //            break;
+            //    }
 
 
 
--- TodoAssignment/Pages/TodoPage.cs
+++ TodoAssignment/Pages/TodoPage.cs
@@ -15,35 +15,64 @@ using OpenQA.Selenium.Interactions.Internal;
 using System.Threading.Tasks;
 using TodoAssignment.Common;
 using NUnit.Framework;
+using OpenQA.Selenium.Support.PageObjects;
 
 namespace TodoAssignment
 {
     public class TodoPage
     {
-        string newtodo = "new-todo";
-        string LabelXpath = ".//*[@id='todo-list']/li/div/label";
-        string labelxpath2=".//*[@id='todo-list']/li/form/input";
-        
-     
+
+        public TodoPage()
+        {
+            PageFactory.InitElements(PropertiesCollection.driver, this);
+        }
+        //string newtodo = "new-todo";
+        //string LabelXpath = ".//*[@id='todo-list']/li/div/label";
+        //string labelxpath2=".//*[@id='todo-list']/li/form/input";
+        [FindsBy(How =How.Id,Using="new-todo")]
+        public IWebElement newtodo { get; set; }
+        [FindsBy(How =How.XPath,Using = ".//*[@id='todo-list']/li/div/label")]
+        public IWebElement LabelXpath { get; set; }
+        [FindsBy(How =How.XPath,Using = ".//*[@id='todo-list']/li/form/input")]
+        public IWebElement labelxpath2 { get; set; }
+
         public void AddTodo()
         {
-            SeleniumWrapper.EnterFillTextBox(newtodo, "FirstTodo", IdentityType.Id);
+            //SeleniumWrapper.EnterFillTextBox(newtodo, "FirstTodo", IdentityType.Id);
+            //Thread.Sleep(3000);
+            //SeleniumWrapper.DoubleClickElement(LabelXpath, IdentityType.Xpath);
+            //string text =SeleniumWrapper.GetText(labelxpath2, IdentityType.Xpath);
+            //Thread.Sleep(3000);
+            //Assert.IsTrue(string.Equals(text, "FirstTodo",StringComparison.InvariantCultureIgnoreCase),"Not Equal");
+            //Thread.Sleep(3000);
+            SeleniumWrapper.EnterFillTextBox(newtodo,"FirstTodo");
             Thread.Sleep(3000);
-            SeleniumWrapper.DoubleClickElement(LabelXpath, IdentityType.Xpath);
-            string text =SeleniumWrapper.GetText(labelxpath2, IdentityType.Xpath);
+            SeleniumWrapper.DoubleClickElement(LabelXpath);
+            string text = SeleniumWrapper.GetText(labelxpath2);
             Thread.Sleep(3000);
-            Assert.IsTrue(string.Equals(text, "FirstTodo",StringComparison.InvariantCultureIgnoreCase),"Not Equal");
+            Assert.IsTrue(string.Equals(text, "FirstTodo", StringComparison.InvariantCultureIgnoreCase), "Not Equal");
             Thread.Sleep(3000);
+
+
         }
         public void EditTodo()
         {
-            SeleniumWrapper.DoubleClickElement(LabelXpath, IdentityType.Xpath);
-           // SeleniumWrapper.ClearTextBox(labelxpath2, IdentityType.Xpath);
-            SeleniumWrapper.FilltextBox(labelxpath2, "123", IdentityType.Xpath);
-            SeleniumWrapper.DoubleClickElement(LabelXpath, IdentityType.Xpath);
-            string text = SeleniumWrapper.GetText(labelxpath2, IdentityType.Xpath);
+           // SeleniumWrapper.DoubleClickElement(LabelXpath, IdentityType.Xpath);
+           //// SeleniumWrapper.ClearTextBox(labelxpath2, IdentityType.Xpath);
+           // SeleniumWrapper.FilltextBox(labelxpath2, "123", IdentityType.Xpath);
+           // SeleniumWrapper.DoubleClickElement(LabelXpath, IdentityType.Xpath);
+           // string text = SeleniumWrapper.GetText(labelxpath2, IdentityType.Xpath);
+           // Assert.IsTrue(string.Equals(text, "FirstTodo123", StringComparison.InvariantCultureIgnoreCase), "Not Equal");
+           // Thread.Sleep(3000);
+
+            SeleniumWrapper.DoubleClickElement(LabelXpath);
+            // SeleniumWrapper.ClearTextBox(labelxpath2, IdentityType.Xpath);
+            SeleniumWrapper.FilltextBox(labelxpath2, "123");
+            SeleniumWrapper.DoubleClickElement(LabelXpath);
+            string text = SeleniumWrapper.GetText(labelxpath2);
             Assert.IsTrue(string.Equals(text, "FirstTodo123", StringComparison.InvariantCultureIgnoreCase), "Not Equal");
             Thread.Sleep(3000);
+
         }
     }
 }
